d17f21ab2dd81d38d26212d57a2da3f35b82d8ba
harshitha <harshnarasimhan@gmail.com>
2020-12-31 15:08:57 +0530

    sling

---
+++ Slingshot.js
@@ -0,0 +1,35 @@
+class Slingshot{
+constructor(bodyA,pointB){
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.04,
+        length:1
+    }
+    this.string=Constraint.create(options);
+    this.pointB=pointB;
+    World.add(world,this.string)
+
+}
+fly(){
+this.string.bodyA=null;
+}
+
+display(){
+    if(this.string.bodyA){
+        var pointA=this.string.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(4);
+        stroke("red")
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
+
+
+
+
+
+
+
+
+}
