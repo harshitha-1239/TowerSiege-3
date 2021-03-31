b94529ef2e72d7c62b8f4a2aef00f94fcc62fd8c
harshitha <harshnarasimhan@gmail.com>
2021-03-31 15:34:54 +0530

    block

--- Block.js
+++ Block.js
@@ -5,6 +5,7 @@ constructor(x,y,width,height){
         restitution:0.4,
         friction:0.0
     }
+    this.visibility=255;
     this.body=Bodies.rectangle(x,y,width,height,options)
     this.width=width;
     this.height=height;
@@ -12,6 +13,7 @@ constructor(x,y,width,height){
 }
 
 display(){
+    if(this.body.speed<3){
     var angle=this.body.angle;
     var pos=this.body.position;
     push()
@@ -20,6 +22,13 @@ display(){
     rectMode(CENTER);
     rect(0,0,this.width,this.height);
     pop();
+    }
+    else{
+        World.remove(world,this.body);
+        push();
+        this.visibility=this.visibility-5;
+        pop();
+    }
 }
 
 
